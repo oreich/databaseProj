1e53f6a3fa15f666e4935cfbd5a395d63e10bcc3
Oz Reich <ozozoz8383@gmail.com>
2023-05-29 15:58:21 +0300

    with play_in new

--- play_in.py
+++ play_in.py
@@ -24,18 +24,15 @@ from datetime import datetime, timedelta
 
 for i in range(n):
     team_ID = fake.random_int(min=1, max=500)
-    player_ID = i +1
+    player_ID = i + 1
     salary = fake.random_int(min=100000, max=10000000)
     start_year = random.randint(1946, 2023)
+    play_in_Id = i + 1
 
-
-
-    data.append({'player_ID': player_ID,'team_ID': team_ID, 'salary': salary,
-                 'start_year': start_year})
+    data.append({'play_in_Id': play_in_Id, 'team_ID': team_ID, 'salary': salary,
+                 'start_year': start_year,'player_ID': player_ID })
 
 # Create a pandas DataFrame from the generated data
 df = pd.DataFrame(data)
 # Write dataframe to CSV file
 df.to_csv('play_in.csv', index=False)
-
-
