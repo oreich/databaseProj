0a917768b920b7fd63cbb75e651ee103a6520403
Oz Reich <ozozoz8383@gmail.com>
2023-06-18 19:57:27 +0300

    get top update1

--- Procedurs/get_top_players.sql
+++ Procedurs/get_top_players.sql
@@ -1,21 +1,36 @@
-CREATE OR REPLACE PROCEDURE get_top_players
+CREATE OR REPLACE PROCEDURE get_top_players(p_result OUT SYS_REFCURSOR)
 IS
 BEGIN
-    -- Retrieve the top three players based on average score
-    FOR player_rec IN (
+    -- Open the cursor for the top players based on average score
+    OPEN p_result FOR
         SELECT thrower_ID, AVG(score) AS average_score
         FROM Throw
         GROUP BY thrower_ID
-        ORDER BY AVG(score) DESC
-    ) LOOP
-        DBMS_OUTPUT.PUT_LINE('Player ID: ' || player_rec.thrower_ID || ', Average Score: ' || player_rec.average_score);
-    END LOOP;
+        ORDER BY AVG(score) DESC;
 EXCEPTION
     WHEN OTHERS THEN
         DBMS_OUTPUT.PUT_LINE('An error occurred while retrieving the top players.');
 END;
 /
+
+DECLARE
+    l_result SYS_REFCURSOR;
+    l_thrower_ID Throw.thrower_ID%TYPE;
+    l_average_score Throw.score%TYPE;
 BEGIN
-    get_top_players;  -- Retrieve the top players based on average score
+    get_top_players(l_result);  -- Retrieve the top players based on average score
+
+    LOOP
+        FETCH l_result INTO l_thrower_ID, l_average_score;
+        EXIT WHEN l_result%NOTFOUND;
+
+        -- Process each row from the result set
+        DBMS_OUTPUT.PUT_LINE('Player ID: ' || l_thrower_ID || ', Average Score: ' || l_average_score);
+    END LOOP;
+
+    CLOSE l_result;
+EXCEPTION
+    WHEN OTHERS THEN
+        DBMS_OUTPUT.PUT_LINE('An error occurred while processing the result set.');
 END;
 /
