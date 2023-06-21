2b89c5044510f4a6942ed86fe19caf00fcddb5b1
Oz Reich <106114471+oreich@users.noreply.github.com>
2023-06-22 01:52:41 +0300

    Update change_team_function.sql

--- Functions/change_team_function.sql
+++ Functions/change_team_function.sql
@@ -1,21 +1,20 @@
 CREATE OR REPLACE FUNCTION change_player_team(
-    p_player_id   IN play_in.player_id%TYPE,
-    p_game_id     IN play_in.game_id%TYPE,
-    p_new_team    IN VARCHAR2
+    p_play_in_id  IN play_in.play_in_id%TYPE,
+    p_team_id     IN play_in.team_id%TYPE
 ) RETURN VARCHAR2
 IS
     l_result VARCHAR2(100);
 BEGIN
     -- Update the team of the player in the play_in table
     UPDATE play_in
-    SET team = p_new_team
-    WHERE player_id = p_player_id AND game_id = p_game_id;
+    SET team_id = p_team_id
+    WHERE play_in_id = p_play_in_id;
 
     l_result := 'Player team changed successfully.';
     RETURN l_result;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
-        l_result := 'Player or game not found.';
+        l_result := 'Play_in ID not found.';
         RETURN l_result;
     WHEN OTHERS THEN
         l_result := 'An error occurred while changing the player team.';
@@ -26,9 +25,8 @@ DECLARE
   result VARCHAR2(100);
 BEGIN
   result := change_player_team(
-    1, -- Player ID
-    123, -- Game ID
-    'New Team Name' -- New team value
+    123, -- Play_in ID
+    456 -- New team ID
   );
   DBMS_OUTPUT.PUT_LINE(result);
 END;
