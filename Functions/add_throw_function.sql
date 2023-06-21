94bab013d5215d7df6bf16dc8fbabc7869498cf8
Oz Reich <ozozoz8383@gmail.com>
2023-06-21 22:47:54 +0300

    with function insert

--- Functions/add_throw_function.sql
+++ Functions/add_throw_function.sql
@@ -1,19 +1,24 @@
 CREATE OR REPLACE FUNCTION add_throw(
-    p_player_id   IN NUMBER,
-    p_game_id     IN NUMBER,
-    p_throw_time  IN NUMBER,
-    p_throw_score IN NUMBER
+    thrower_id   IN NUMBER,
+    game_id     IN NUMBER,
+    time  IN NUMBER,
+    score IN NUMBER,
+    throw_type IN VARCHAR2,
+    style_throw IN VARCHAR2,
+    location IN VARCHAR2
+    thrower_id   IN NUMBER,
 ) RETURN VARCHAR2
 IS
     l_result VARCHAR2(100);
 BEGIN
     -- Check if throw time is within the valid range (1-48)
-    IF p_throw_time >= 1 AND p_throw_time <= 48 THEN
+    IF time >= 1 AND time <= 48 THEN
         -- Check if throw score is within the valid range (0-3)
-        IF p_throw_score >= 0 AND p_throw_score <= 3 THEN
+        IF score >= 0 AND score <= 3 THEN
             -- Insert the throw into the database
-            INSERT INTO throws (player_id, game_id, throw_time, throw_score)
-            VALUES (p_player_id, p_game_id, p_throw_time, p_throw_score);
+            INSERT INTO throw (throw_ID,throw_type,location,time,thrower_id, score,game_id, style_throw)
+            VALUES (throw_ID,throw_type,location,time,thrower_id, score,game_id, style_throw);
+
 
             l_result := 'Throw added successfully.';
         ELSE
@@ -35,7 +40,7 @@ END;
 DECLARE
     l_result VARCHAR2(100);
 BEGIN
-    l_result := add_throw(123, 456, 10, 2);  -- Example: Add a throw for player ID 123, game ID 456, at time 10 with a score of 2
+    l_result := add_throw(123,'hook', 'paint',45, 1, 1, 'Free Throw', 124, 'tip in');
     DBMS_OUTPUT.PUT_LINE(l_result);
 END;
 /
