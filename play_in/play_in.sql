3fc935e5c1e0e3d32ac3d490cc64ba4d0c1aab0c
Oz Reich <106114471+oreich@users.noreply.github.com>
2023-06-22 01:37:31 +0300

    Update play_in.sql

--- play_in/play_in.sql
+++ play_in/play_in.sql
@@ -1,10 +1,11 @@
 CREATE TABLE play_in
 (
+  play_in_id NOT NULL,
   salary INT NOT NULL,
   start_year INT NOT NULL,
   game_ID INT NOT NULL,
   player_ID INT NOT NULL,
-  PRIMARY KEY (game_ID, player_ID),
+  PRIMARY KEY (play_in_id),
   FOREIGN KEY (game_ID) REFERENCES game(game_ID),
   FOREIGN KEY (player_ID) REFERENCES player(player_ID)
 );
