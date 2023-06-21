eccc6c1617c189c6f31b6f83e898212f289393e7
Oz Reich <106114471+oreich@users.noreply.github.com>
2023-06-22 01:39:17 +0300

    Update change_team_function.sql

--- Functions/change_team_function.sql
+++ Functions/change_team_function.sql
@@ -1,35 +1,35 @@
-CREATE OR REPLACE FUNCTION change_player_team (
-   p_player_id   IN NUMBER,
-   p_new_team_id IN NUMBER
+CREATE OR REPLACE FUNCTION change_player_team(
+    p_player_id   IN play_in.player_id%TYPE,
+    p_game_id     IN play_in.game_id%TYPE,
+    p_new_team    IN VARCHAR2
 ) RETURN VARCHAR2
 IS
-   l_result VARCHAR2(100);
+    l_result VARCHAR2(100);
 BEGIN
-   -- Update the player's team
-   UPDATE play_in
-   SET chashken.team_id = p_new_team_id
-   WHERE liocihen.player.id = p_player_id;
+    -- Update the team of the player in the play_in table
+    UPDATE play_in
+    SET team = p_new_team
+    WHERE player_id = p_player_id AND game_id = p_game_id;
 
-   -- Check if the update was successful
-   IF SQL%ROWCOUNT > 0 THEN
-      l_result := 'Player team updated successfully.';
-   ELSE
-      l_result := 'Player team update failed. Player ID or team ID not found.';
-   END IF;
-
-   RETURN l_result;
+    l_result := 'Player team changed successfully.';
+    RETURN l_result;
 EXCEPTION
-   WHEN OTHERS THEN
-      l_result := 'An error occurred while updating the player team.';
-      RETURN l_result;
+    WHEN NO_DATA_FOUND THEN
+        l_result := 'Player or game not found.';
+        RETURN l_result;
+    WHEN OTHERS THEN
+        l_result := 'An error occurred while changing the player team.';
+        RETURN l_result;
 END;
 /
-
-
 DECLARE
-   l_result VARCHAR2(100);
+  result VARCHAR2(100);
 BEGIN
-   l_result := change_player_team(123, 456);  -- Example: Change player ID 123 to team ID 456
-   DBMS_OUTPUT.PUT_LINE(l_result);
+  result := change_player_team(
+    1, -- Player ID
+    123, -- Game ID
+    'New Team Name' -- New team value
+  );
+  DBMS_OUTPUT.PUT_LINE(result);
 END;
 /
